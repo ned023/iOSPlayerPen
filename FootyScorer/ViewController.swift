afa6bdab0f8ae211af3c4b7c38610b468faab78a
Graeme Kelly <graeme@Graemes-iMac.local>
2015-05-16 07:43:01 +1000

    Initial Commit

---
+++ FootyScorer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FootyScorer
+//
+//  Created by Graeme Kelly on 16/05/2015.
+//  Copyright (c) 2015 Graeme Kelly. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
