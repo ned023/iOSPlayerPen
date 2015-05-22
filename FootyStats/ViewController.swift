c0e2ac7bfbea77d7ca9289db981caf57dae5894d
Graeme Kelly <graeme@Graemes-iMac.local>
2015-05-23 09:32:34 +1000

    Added basic scoring

--- FootyStats/ViewController.swift
+++ FootyStats/ViewController.swift
@@ -10,6 +10,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var HomeTeamLabel: UILabel!
+    @IBOutlet weak var HomeGoalsLabel: UILabel!
+    @IBOutlet weak var HomeBehindsLabel: UILabel!
+    @IBOutlet weak var HomePointsLabel: UILabel!
+
+    @IBOutlet weak var awayGoalsLabel: UILabel!
+    @IBOutlet weak var awayBehindsLabel: UILabel!
+    @IBOutlet weak var awayPointsLabel: UILabel!
+    
+    
+    var homeGoals = 0
+    var homeBehinds = 0
+    var homePoints = 0
+    var awayGoals = 0
+    var awayBehinds = 0
+    var awayPoints = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +36,69 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func addHomeGoal() {
+        homeGoals += 1
+        homePoints = homePoints + 6
+        HomeGoalsLabel.text = String(homeGoals)
+        HomePointsLabel.text = String(homePoints)
+    }
+    
+    @IBAction func minusHomeGoal() {
+        if (homeGoals > 0) {
+            homeGoals += -1
+            homePoints = homePoints - 6
+            HomeGoalsLabel.text = String(homeGoals)
+            HomePointsLabel.text = String(homePoints)
+        }
+    }
+    
+    @IBAction func addHomeBehind() {
+        homeBehinds = homeBehinds + 1
+        homePoints = homePoints + 1
+        HomeBehindsLabel.text = String(homeBehinds)
+        HomePointsLabel.text = String(homePoints)
+    }
+    
+    @IBAction func minusHomeBehind() {
+        if (homeBehinds > 0) {
+            homeBehinds += -1
+            homePoints = homePoints - 1
+            HomeBehindsLabel.text = String(homeBehinds)
+            HomePointsLabel.text = String(homePoints)
+        }
+    }
+    
+    @IBAction func addAwayGoal() {
+        awayGoals += 1
+        awayPoints = awayPoints + 6
+        awayGoalsLabel.text = String(awayGoals)
+        awayPointsLabel.text = String(awayPoints)
+    }
+    
+    @IBAction func minusAwayGoal() {
+        if (awayGoals > 0) {
+            awayGoals += -1
+            awayPoints = awayPoints - 6
+            awayGoalsLabel.text = String(awayGoals)
+            awayPointsLabel.text = String(awayPoints)
+        }
+    }
+    
+    @IBAction func addAwayBehind() {
+        awayBehinds = awayBehinds + 1
+        awayPoints = awayPoints + 1
+        awayBehindsLabel.text = String(awayBehinds)
+        awayPointsLabel.text = String(awayPoints)
+    }
+    
+    @IBAction func minusAwayBehind() {
+        if (awayBehinds > 0) {
+            awayBehinds += -1
+            awayPoints = awayPoints - 1
+            awayBehindsLabel.text = String(awayBehinds)
+            awayPointsLabel.text = String(awayPoints)
+        }
+    }
 }
 
